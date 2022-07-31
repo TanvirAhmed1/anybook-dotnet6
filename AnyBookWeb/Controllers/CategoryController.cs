e88b9cb52ad4bd1b8c377851dab7987666bc5793
Tanvir Ahmed <tanvir.ahmed@beacontechbd.com>
2022-07-31 11:39:12 +0600

    CategoryViewAdded

--- AnyBookWeb/Controllers/CategoryController.cs
+++ AnyBookWeb/Controllers/CategoryController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AnyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AnyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            var objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
